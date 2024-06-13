b37ed259c8c6bafe6cb93118c2579aa458832846
Kenta-kenpy <101815272+Kenta-kenpy@users.noreply.github.com>
2024-06-13 17:19:11 +0900

    Create index.py

---
+++ index.py
@@ -0,0 +1,13 @@
+html = '''<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Progate</title>
+  </head>
+  <body>
+    <h1 class="title">Python3 Paizaラーニング</h1>
+    <a href="https://github.com/Kenta-kenpy/paiza-python3">python3 標準入力備忘録</a>
+  </body>
+</html>'''
+
+print(html)
